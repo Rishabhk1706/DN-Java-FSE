601944c4b85907a3704816f0ed1cc2fa00596cb3
Rishabhk1706 <rishabhk1706@gmail.com>
2026-06-30 23:49:59 +0530

    Mockito Exercise All

---
+++ Week2/TDD using JUnit5 and Mockito/3. Mockito Exercises/Mockito/src/main/java/com/example/mockito/api/ExternalApi.java
@@ -0,0 +1,5 @@
+package com.example.mockito.api;
+
+public interface ExternalApi {
+    String getData();
+}
---
+++ Week2/TDD using JUnit5 and Mockito/3. Mockito Exercises/Mockito/src/test/java/com/example/mockito/service/MyServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.mockito.service;
+
+import com.example.mockito.api.ExternalApi;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+
+    @Test
+    void testFetchData() {
+
+        // Create Mock
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        // Stub Method
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        // Inject Mock
+        MyService service = new MyService(mockApi);
+
+        // Call Method
+        String result = service.fetchData();
+
+        // Assertion
+        assertEquals("Mock Data", result);
+
+    }
+
+    @Test
+    void testVerifyInteraction() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
+}
