355f0a6d3e994550adc4ccb8111845eafbdb4176
Rishabhk1706 <rishabhk1706@gmail.com>
2026-07-07 20:08:24 +0530

    Decorator Pattern

---
+++ Week1/DesignPrinciplesAndPatterns/DecoratorPatternExample/DecoratorPatternTest.java
@@ -0,0 +1,70 @@
+interface Notifier {
+    void send(String message);
+}
+
+class EmailNotifier implements Notifier {
+    @Override
+    public void send(String message) {
+        System.out.println("Sending Email: " + message);
+    }
+}
+
+abstract class NotifierDecorator implements Notifier {
+    protected Notifier notifier;
+
+    public NotifierDecorator(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    @Override
+    public void send(String message) {
+        notifier.send(message);
+    }
+}
+
+class SMSNotifierDecorator extends NotifierDecorator {
+
+    public SMSNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+class SlackNotifierDecorator extends NotifierDecorator {
+
+    public SlackNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        System.out.println("Sending Slack Message: " + message);
+    }
+}
+
+public class DecoratorPatternTest {
+    public static void main(String[] args) {
+
+        System.out.println("Email Notification:");
+        Notifier emailNotifier = new EmailNotifier();
+        emailNotifier.send("System Update Available");
+
+        System.out.println("\nEmail + SMS Notification:");
+        Notifier emailAndSMS =
+                new SMSNotifierDecorator(new EmailNotifier());
+        emailAndSMS.send("System Update Available");
+
+        System.out.println("\nEmail + SMS + Slack Notification:");
+        Notifier allChannels =
+                new SlackNotifierDecorator(
+                        new SMSNotifierDecorator(
+                                new EmailNotifier()));
+        allChannels.send("System Update Available");
+    }
+}
