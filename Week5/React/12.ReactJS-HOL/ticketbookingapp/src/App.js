34014d0c1618aeadcff4178fc79040f8e05609d2
Rishabhk1706 <rishabhk1706@gmail.com>
2026-07-22 21:21:17 +0530

    React HandsOn12 ticket booking app

---
+++ Week5/React/12.ReactJS-HOL/ticketbookingapp/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Greeting from "./components/Greeting";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div className="container">
+      <Greeting isLoggedIn={isLoggedIn} />
+
+      {isLoggedIn ? (
+        <LogoutButton onClick={handleLogout} />
+      ) : (
+        <LoginButton onClick={handleLogin} />
+      )}
+    </div>
+  );
+}
+
+export default App;
